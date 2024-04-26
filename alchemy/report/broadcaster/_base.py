3dd3787b087318d6c73635ab1ac13b3ccdb3f4bd
cwognum <caswognum@outlook.com>
2024-04-26 12:46:19 -0400

    Add HTML broadcaster

--- alchemy/report/broadcaster/_base.py
+++ alchemy/report/broadcaster/_base.py
@@ -18,10 +18,19 @@ class ReportBroadcaster(abc.ABC):
         self.on_report_start(self._report)
         for section in self._report.sections:
             self.on_section_start(section)
-            for log in section.logs:
-                self.render_log(log)
-            for image in section.images:
-                self.render_image(image)
+
+            if len(section.logs) > 0:
+                self.on_logs_start()
+                for log in section.logs:
+                    self.render_log(log)
+                self.on_logs_end()
+
+            if len(section.images) > 0:
+                self.on_images_start()
+                for image in section.images:
+                    self.render_image(image)
+                self.on_images_end()
+
             self.on_section_end(section)
         self.on_report_end(self._report)
 
@@ -37,6 +46,18 @@ class ReportBroadcaster(abc.ABC):
         pass
 
     def on_section_end(self, section: Section):
+        pass
+
+    def on_logs_start(self):
+        pass
+
+    def on_logs_end(self):
+        pass
+
+    def on_images_start(self):
+        pass
+
+    def on_images_end(self):
         pass
 
     def on_report_start(self, report: CurationReport):
